3041990163c3b9b692fe8536fa1c25370a67a669
daydreamore <sven.guenther1992@gmail.com>
2022-11-04 18:40:23 +0100

    homework2 submission

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -2,18 +2,33 @@
 
 
 uint64_t factorial(uint64_t val) {
-    // TODO
-    return 0;
+    uint64_t count = 1;
+    for (int i = 0; i < val+1; i++){
+        if (i > 0){
+            count *= i;
+        }
+    }
+    return count;
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+    if (val<val2){
+        return 0;
+    } else {
+        uint64_t out_perm = factorial(val)/factorial(val-val2);
+        return out_perm;
+    }
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+    if (val-val2 < 0){
+        return 0;
+    } else if (factorial(val2)*factorial(val-val2) <= 0) {
+        return 0;
+    } else {
+        uint64_t out_comb = factorial(val)/(factorial(val2)*factorial(val-val2));
+        return out_comb;
+    }
 }
